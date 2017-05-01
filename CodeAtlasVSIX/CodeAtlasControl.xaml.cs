22e8e6b876666a2987dd2a8f3e63d7c94beed10d
league1991 <549088764@qq.com>
2017-05-01 21:35:27 +0800

    Initial commit

---
+++ CodeAtlasVSIX/CodeAtlasControl.xaml.cs
@@ -0,0 +1,57 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="CodeAtlasControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace CodeAtlasVSIX
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Interaction logic for CodeAtlasControl.
+    /// </summary>
+    public partial class CodeAtlasControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CodeAtlasControl"/> class.
+        /// </summary>
+        public CodeAtlasControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "CodeAtlas");
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.canvas.Children.Add(new CodeUIItem());
+        }
+
+        private void onMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            Point position = e.GetPosition(this.canvas);
+            scaleValue += e.Delta * 0.005;
+            ScaleTransform scale = new ScaleTransform(scaleValue, scaleValue, position.X, position.Y);
+            this.canvas.LayoutTransform = scale;
+            this.canvas.UpdateLayout();
+        }
+
+        private double scaleValue = 1.0;
+    }
+}
---
+++ CodeAtlasVSIX/CodeUIItem.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CodeAtlasVSIX
+{
+    /// <summary>
+    /// Interaction logic for CodeUIItem.xaml.
+    /// </summary>
+    [ProvideToolboxControl("CodeAtlasVSIX.CodeUIItem", true)]
+    public partial class CodeUIItem : UserControl
+    {
+        public CodeUIItem()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(string.Format(CultureInfo.CurrentUICulture, "We are inside {0}.Button1_Click()", this.ToString()));
+        }
+
+        private void circle_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+        }
+
+        private void circle_DragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void circle_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MessageBox.Show(string.Format(CultureInfo.CurrentUICulture, "We are inside {0}.Button1_Click()", this.ToString()));
+        }
+    }
+}
