95afcdef32795a50b93952d0c400e6b9d32ff9b2
league1991 <549088764@qq.com>
2017-05-06 21:08:57 +0800

    Add MainUI

--- CodeAtlasVSIX/CodeAtlas.cs
+++ CodeAtlasVSIX/CodeAtlas.cs
@@ -35,7 +35,8 @@ namespace CodeAtlasVSIX
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
             // the object returned by the Content property.
-            this.Content = new CodeView();
+            //this.Content = new CodeView();
+            this.Content = new MainUI();
         }
     }
 }
---
+++ CodeAtlasVSIX/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAtlasVSIX
+{
+    class UIManager
+    {
+        CodeScene scene = null;
+        MainUI mainUI = null;
+        static UIManager uiMgr = null;
+
+        public UIManager()
+        {
+            scene = new CodeScene();
+        }
+
+        public static UIManager Instance()
+        {
+            if (uiMgr != null)
+            {
+                uiMgr = new UIManager();
+            }
+            return uiMgr;
+        }
+
+        public MainUI GetMainUI()
+        {
+            return mainUI;
+        }
+
+        public CodeScene GetScene()
+        {
+            return scene;
+        }
+        
+    }
+}
