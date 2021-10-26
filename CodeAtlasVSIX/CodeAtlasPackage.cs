9d6d782e1ca972dd0216b2fce8532d95955daf53
Yaobin Ouyang <yaobino@nvidia.com>
2021-10-26 13:07:07 -0400

    Upgrade version to 1.3.34

--- CodeAtlasVSIX/CodeAtlasPackage.cs
+++ CodeAtlasVSIX/CodeAtlasPackage.cs
@@ -41,7 +41,7 @@ namespace CodeAtlasVSIX
     /// </para>
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [InstalledProductRegistration("#110", "#112", "1.3.33", IconResourceID = 400)] // Info on this package for Help/About
+    [InstalledProductRegistration("#110", "#112", "1.3.34", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideToolWindow(typeof(CodeAtlas))]
     [Guid(CodeAtlasPackage.PackageGuidString)]
